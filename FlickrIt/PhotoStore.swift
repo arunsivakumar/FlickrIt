7359f042fc45236d137e7e9b56a1d05cf00e537f
Arun Sivakumar <arunsivakumar@live.com>
2017-08-03 15:48:50 -0600

    Add PhotoStore

---
+++ FlickrIt/PhotoStore.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoStore.swift
+//  FlickrIt
+//
+//  Created by Lakshmi on 8/3/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum PhotosResult{
+    case success([Photo])
+    case failure(Error)
+}
+class PhotoStore{
+    
+    
+}
