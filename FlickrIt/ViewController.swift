5443e0521694c23316f93a5db835743a99a97fd5
Arun Sivakumar <arunsivakumar@live.com>
2017-08-03 15:36:09 -0600

    Initial Commit

---
+++ FlickrIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrIt
+//
+//  Created by Lakshmi on 8/3/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
