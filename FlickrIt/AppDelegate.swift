4cfefcc169129b15950e1bd39646981f3d84c05e
Arun Sivakumar <arunsivakumar@live.com>
2017-08-03 20:12:18 -0600

    Add dependency injection

--- FlickrIt/AppDelegate.swift
+++ FlickrIt/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Dependancy injection for Photos View Controller
+        
+        let rootVC = window!.rootViewController as! UINavigationController
+        let photosVC = rootVC.topViewController as! PhotosViewController
+        photosVC.photoStore = PhotoStore()
+
+        
         return true
     }
 
