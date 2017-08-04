a4715819bfb9b3a65c46d1359730321983366443
Arun Sivakumar <arunsivakumar@live.com>
2017-08-03 20:16:17 -0600

    Add Swift bonds

--- Pods/Target Support Files/Pods-FlickrIt/Pods-FlickrIt-frameworks.sh
+++ Pods/Target Support Files/Pods-FlickrIt/Pods-FlickrIt-frameworks.sh
@@ -90,10 +90,16 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Bond/Bond.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Diff/Diff.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveKit/ReactiveKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Bond/Bond.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Diff/Diff.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveKit/ReactiveKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
